63bff356659724325175877b718c2103318f23e1
Anthony Pham <80490069+phamofanthony@users.noreply.github.com>
2025-03-08 01:22:21 -0600

    initial implementation (router compiles, neither has been tested)

--- exercises/internet-router/control-plane/pwospf.py
+++ exercises/internet-router/control-plane/pwospf.py
@@ -1,13 +1,12 @@
 from select import select
-from scapy.all import conf, ETH_P_ALL, MTU, plist, Packet, Ether, IP, ARP
+from scapy.all import conf, ETH_P_ALL, MTU, plist, Packet, Ether, IP, ARP, sendp, ByteField, ShortField, IntField
 from scapy.packet import Packet, bind_layers
 from threading import Thread
-
+import time
 
 ARP_OP_REPLY = 0x0002
 ARP_OP_REQ = 0x0001
 ARP_TIMEOUT = 30
-HELLO_TYPE = 0x01
 HELLO_TYPE = 0x01
 ICMP_ECHO_REPLY_CODE = 0x00
 ICMP_ECHO_REPLY_TYPE = 0x00
@@ -16,10 +15,8 @@ ICMP_HOST_UNREACHABLE_TYPE = 0x03
 ICMP_PROT_NUM = 0x01
 LSU_TYPE = 0x04
 OSPF_PROT_NUM = 0x59
-OSPF_PROT_NUM = 0x59
 PWOSPF_HELLO_DEST = '224.0.0.5'
 TYPE_CPU_METADATA = 0x080a
-
 
 def sniff(store=False, prn=None, lfilter=None, stop_event=None, refresh=.1, *args, **kwargs):
     s = conf.L2listen(type=ETH_P_ALL, *args, **kwargs)
@@ -47,22 +44,40 @@ def sniff(store=False, prn=None, lfilter=None, stop_event=None, refresh=.1, *arg
         s.close()
     return plist.PacketList(lst, "Sniffed")
 
-
+# Control plane: Interface object holds basic info and neighbor table.
 class Interface():
     def __init__(self, addr, mask, helloint, port):
-        # TODO: Handle neighbors
+        # Handle neighbors: initialize an empty neighbor table.
+        self.addr = addr
+        self.mask = mask
+        self.helloint = helloint
+        self.port = port  # This should be the name of the system interface for sending/receiving.
+        self.neighbors = {}  # Dictionary to store neighbor information keyed by routerID.
 
-
+# ARPManager: Thread to process ARP packets.
 class ARPManager(Thread):
     def __init__(self, cntrl):
         super(ARPManager, self).__init__()
         self.cntrl = cntrl
 
     def run(self):
-        # TODO: Handle ARP packets
-        pass
+        # Handle ARP packets: Listen for ARP requests and send replies.
+        def arp_filter(p):
+            return ARP in p
+        def process_arp(p):
+            arp_pkt = p[ARP]
+            if arp_pkt.op == ARP_OP_REQ:
+                print("Received ARP request for", arp_pkt.pdst)
+                # Create a simple ARP reply using the controller's MAC.
+                arp_reply = ARP(op=ARP_OP_REPLY, hwsrc=self.cntrl.MAC, psrc=arp_pkt.pdst,
+                                hwdst=arp_pkt.hwsrc, pdst=arp_pkt.psrc)
+                ether = Ether(dst=arp_pkt.hwsrc, src=self.cntrl.MAC, type=0x806)
+                reply = ether / arp_reply
+                sendp(reply, iface=self.cntrl.iface, verbose=False)
+                print("Sent ARP reply for", arp_pkt.pdst)
+        sniff(prn=process_arp, lfilter=arp_filter)
 
-
+# HelloManager: Thread to send periodic Hello messages.
 class HelloManager(Thread):
     def __init__(self, cntrl, intf):
         super(HelloManager, self).__init__()
@@ -70,10 +85,20 @@ class HelloManager(Thread):
         self.intf = intf
 
     def run(self):
-        # TODO: Handle Hello packets
-        pass
+        # Handle Hello packets: Periodically send Hello packets out on the interface.
+        while True:
+            hello_pkt = (Ether(src=self.cntrl.MAC, dst="01:00:5e:00:00:05") /
+                         IP(dst=PWOSPF_HELLO_DEST, proto=OSPF_PROT_NUM) /
+                         PWOSPF(type=HELLO_TYPE, routerID=self.cntrl.routerID, areaID=self.cntrl.areaID) /
+                         Hello(hello_interval=self.intf.helloint,
+                               dead_interval=self.intf.helloint * 4,
+                               router_priority=1,
+                               reserved=0))
+            sendp(hello_pkt, iface=self.intf.port, verbose=False)
+            print("Sent Hello packet on interface", self.intf.port)
+            time.sleep(self.intf.helloint)
 
-
+# LSUManager: Thread to process Link State Update packets.
 class LSUManager(Thread):
     def __init__(self, cntrl, lsuint):
         super(LSUManager, self).__init__()
@@ -81,51 +106,95 @@ class LSUManager(Thread):
         self.cntrl = cntrl
 
     def run(self):
-        # TODO: Handle LSU packets
+        # Handle LSU packets: Listen for LSU packets and process them.
+        def lsu_filter(p):
+            return p.haslayer(LSU)
+        def process_lsu(p):
+            lsu_pkt = p[LSU]
+            print("Received LSU packet with sequence", lsu_pkt.sequence)
+            # Here, update routing table based on LSU info (not fully implemented).
+        sniff(prn=process_lsu, lfilter=lsu_filter)
 
-
+# RouterController: Main control plane thread for PWOSPF.
 class RouterController(Thread):
     def __init__(self, sw, routerID, MAC, areaID, intfs, lsuint=2, start_wait=0.3):
-        # TODO: Create a PWOSPF Controller
+        # Create a PWOSPF Controller.
         super(RouterController, self).__init__()
-        pass
+        self.sw = sw
+        self.routerID = routerID
+        self.MAC = MAC
+        self.areaID = areaID
+        self.intfs = intfs  # List of Interface objects.
+        self.lsuint = lsuint
+        self.start_wait = start_wait
+        self.routing_table = {}  # Simple routing table (for demonstration).
+        # Assume a primary interface for control messages.
+        self.iface = intfs[0].port
 
+    def run(self):
+        print("Starting PWOSPF Router Controller with RouterID:", self.routerID)
+        # Start ARP manager.
+        arp_manager = ARPManager(self)
+        arp_manager.daemon = True
+        arp_manager.start()
+        # Start Hello managers for each interface.
+        for intf in self.intfs:
+            hello_manager = HelloManager(self, intf)
+            hello_manager.daemon = True
+            hello_manager.start()
+        # Start LSU manager.
+        lsu_manager = LSUManager(self, self.lsuint)
+        lsu_manager.daemon = True
+        lsu_manager.start()
+        # Main loop: periodically print routing table.
+        while True:
+            print("Current routing table:", self.routing_table)
+            time.sleep(10)
+
+# Packet definitions for control plane protocols using Scapy.
 
 class CPUMetadata(Packet):
     name = "CPUMetadata"
     fields_desc = [
-        # TODO: Create CPUMetadata packet fields
+        ShortField("origEtherType", 0)
     ]
-
 
 class PWOSPF(Packet):
     name = "PWOSPF"
     fields_desc = [
-        # TODO: Create PWOSPF packet fields
+        ByteField("type", 0),         # PWOSPF type: e.g., HELLO or LSU
+        IntField("routerID", 0),       # Router identifier
+        IntField("areaID", 0)          # Area identifier
     ]
-
 
 class Hello(Packet):
     name = "Hello"
     fields_desc = [
-        # TODO: Create Hello packet fields
+        ShortField("hello_interval", 0),  # Interval in seconds between HELLO messages
+        ShortField("dead_interval", 0),   # Time until neighbor declared dead
+        ByteField("router_priority", 0),  # Router priority
+        ByteField("reserved", 0)          # Reserved field for alignment
     ]
-
 
 class LSUad(Packet):
     name = "LSUad"
     fields_desc = [
-        # TODO: Create LSUad packet fields
+        IntField("link_id", 0),   # Identifier for the advertised link
+        ShortField("cost", 0)     # Cost of the link
     ]
-
 
 class LSU(Packet):
     name = "LSU"
     fields_desc = [
-        # TODO: Create LSU packet fields
+        IntField("sequence", 0),           # Sequence number for LSU messages
+        ByteField("num_advertisements", 0),  # Number of link advertisements
+        # Three reserved bytes for padding/alignment
+        ByteField("reserved1", 0),
+        ByteField("reserved2", 0),
+        ByteField("reserved3", 0)
     ]
 
-
+# Bind protocol layers for packet dissection.
 bind_layers(Ether, CPUMetadata, type=TYPE_CPU_METADATA)
 bind_layers(CPUMetadata, IP, origEtherType=0x0800)
 bind_layers(CPUMetadata, ARP, origEtherType=0x0806)
--- exercises/internet-router/data-plane/router.p4
+++ exercises/internet-router/data-plane/router.p4
@@ -1,55 +1,65 @@
 /* -*- P4_16 -*- */
+
 #include <core.p4>
 #include <v1model.p4>
 
-typedef bit<9>  port_t;
-typedef bit<48> macAddr_t;
-typedef bit<32> ip4Addr_t;
-typedef bit<128> ip6Addr_t;
-typedef bit<16> mcastGrp_t;
-
-const macAddr_t BROADCAST_ADDR  = 0xffffffffffff;
-const mcastGrp_t BROADCAST_MGID = 0x0001;
-
-const ip4Addr_t ALLSPFROUTERS_ADDR = 0xe0000005;
-
-const port_t CPU_PORT           = 0x1;
-
-const bit<16> ARP_OP_REQ        = 0x0001;
-const bit<16> ARP_OP_REPLY      = 0x0002;
-
-const bit<16> TYPE_IPV4         = 0x0800;
-const bit<16> TYPE_IPV6         = 0x86dd;
-const bit<16> TYPE_ARP          = 0x0806;
+// Define protocol types
+const bit<16> TYPE_ARP         = 0x806;
+const bit<16> TYPE_IPv4        = 0x800;
 const bit<16> TYPE_CPU_METADATA = 0x080a;
 
-// standard Ethernet header
+// Header definitions
 header ethernet_t {
-    macAddr_t dstAddr;
-    macAddr_t srcAddr;
-    bit<16>   etherType;
+    bit<48> dstAddr;
+    bit<48> srcAddr;
+    bit<16> etherType;
 }
 
-//Header for information sent to the CPU
+header arp_t {
+    bit<16> htype;
+    bit<16> ptype;
+    bit<8>  hlen;
+    bit<8>  plen;
+    bit<16> oper;
+    bit<48> sha;
+    bit<32> spa;
+    bit<48> tha;
+    bit<32> tpa;
+}
+
+header ipv4_t {
+    bit<4>  version;
+    bit<4>  ihl;
+    bit<8>  diffserv;
+    bit<16> totalLen;
+    bit<16> identification;
+    bit<3>  flags;
+    bit<13> fragOffset;
+    bit<8>  ttl;
+    bit<8>  protocol;
+    bit<16> hdrChecksum;
+    bit<32> srcAddr;
+    bit<32> dstAddr;
+}
+
 header cpu_metadata_t {
- 
- //TODO: Construct the header with the information to send to the CPU
- //This header should be added to all packets sent to the control plane
- 
+    bit<16> origEtherType;
+    // Additional metadata fields can be added if needed
 }
 
-//TODO: define all other headers required by the router.
-
-
+// Standard metadata and user metadata (if any)
+struct metadata {
+    // (empty for now)
+}
 
 struct headers {
-    ethernet_t        ethernet;
-    cpu_metadata_t    cpu_metadata;
-    //TODO: add all other supported headers
+    ethernet_t      ethernet;
+    arp_t           arp;
+    ipv4_t          ipv4;
+    cpu_metadata_t  cpu_metadata;
 }
 
-struct metadata { }
-
+// Parser: extracts Ethernet and then ARP or IPv4 based on etherType
 parser MyParser(packet_in packet,
                 out headers hdr,
                 inout metadata meta,
@@ -57,64 +67,119 @@ parser MyParser(packet_in packet,
     state start {
         transition parse_ethernet;
     }
-
+    
     state parse_ethernet {
         packet.extract(hdr.ethernet);
-        // TODO: Parse all the headers supported by the project
-        transition accept;
+        transition select(hdr.ethernet.etherType) {
+            TYPE_ARP:  parse_arp;
+            TYPE_IPv4: parse_ipv4;
+            default:   accept;
         }
+    }
+    
+    state parse_arp {
+        packet.extract(hdr.arp);
+        transition accept;
+    }
+    
+    state parse_ipv4 {
+        packet.extract(hdr.ipv4);
+        transition accept;
+    }
+}
+
+// Ingress processing: implement routing logic and special handling for ARP, TTL, and CPU–directed packets.
+control MyIngress(inout headers hdr,
+                  inout metadata meta,
+                  inout standard_metadata_t standard_metadata) {
+    
+    // Drop action: simply mark the packet to be dropped.
+    action drop() {
+        mark_to_drop(standard_metadata);
+    }
+    
+    // Forward action: update Ethernet header and set egress port.
+    action forward(bit<48> newDst, bit<9> port) {
+        hdr.ethernet.srcAddr = hdr.ethernet.dstAddr;
+        hdr.ethernet.dstAddr = newDst;
+        standard_metadata.egress_spec = port;
+    }
+    
+    // Send-to-CPU action: set egress to CPU and append metadata.
+    action send_to_cpu() {
+        // Assume CPU port is reserved (e.g., port 255)
+        standard_metadata.egress_spec = 255;
+        hdr.cpu_metadata.setValid();
+        hdr.cpu_metadata.origEtherType = hdr.ethernet.etherType;
+    }
+    
+    // (Optional) A table for IPv4 routing lookup using LPM
+    table ipv4_routing {
+        key = {
+            hdr.ipv4.dstAddr: lpm;
+        }
+        actions = {
+            forward;
+            drop;
+        }
+        size = 1024;
+        default_action = drop();
+    }
+    
+    apply {
+        if (hdr.ethernet.etherType == TYPE_ARP) {
+            // All ARP packets are sent to the CPU.
+            send_to_cpu();
+        } else if (hdr.ethernet.etherType == TYPE_IPv4) {
+            // Decrement TTL; if TTL becomes zero, drop the packet.
+            if (hdr.ipv4.ttl <= 1) {
+                drop();
+            } else {
+                hdr.ipv4.ttl = hdr.ipv4.ttl - 1;
+                // If the destination IP is local (for example, in 10.0.0.0/8), forward to CPU.
+                if ((hdr.ipv4.dstAddr & 0xff000000) == 0x0a000000) {
+                    send_to_cpu();
+                } else {
+                    // Otherwise, perform routing lookup.
+                    ipv4_routing.apply();
+                }
+            }
+        }
+    }
+}
+
+// Deparser: reassembles the packet (emit headers in order)
+control MyDeparser(packet_out packet, in headers hdr) {
+    apply {
+        packet.emit(hdr.ethernet);
+        packet.emit(hdr.arp);
+        packet.emit(hdr.ipv4);
+        packet.emit(hdr.cpu_metadata);
     }
 }
 
 control MyVerifyChecksum(inout headers hdr, inout metadata meta) {
     apply {
-         // TODO: verify checksum 
+        // do nothing
     }
 }
 
-control MyIngress(inout headers hdr,
-                  inout metadata meta,
-                  inout standard_metadata_t standard_metadata) {
-
-    action set_egr(port_t port) {
-        standard_metadata.egress_spec = port;
-    }
-    
-   action drop() {
-        mark_to_drop(standard_metadata);
-    }
-
-   action send_to_cpu() {
-      //TODO: What should you do here?
-    }
-
-  
-//TODO: Add all tables and actions
-
+control MyEgress (inout headers hdr, inout metadata meta, inout standard_metadata_t standard_metadata) {
     apply {
-        //TODO: Add your control flow
-        //The following is a dummy code that will return the packet "as is" to the source
-        standard_metadata.egress_spec = standard_metadata.ingress_port
+        // do nothing
     }
 }
 
-control MyEgress(inout headers hdr,
-                 inout metadata meta,
-                 inout standard_metadata_t standard_metadata) {
-    apply { }
-}
-
-control MyComputeChecksum(inout headers  hdr, inout metadata meta) {
+control MyComputeChecksum(inout headers hdr, inout metadata meta) {
     apply {
-       // TODO: Update checksum 
+        // do nothing
     }
 }
 
-control MyDeparser(packet_out packet, in headers hdr) {
-    apply {
-        packet.emit(hdr.ethernet);
-       //TODO: Add all other headers
-    }
-}
-
-V1Switch(MyParser(), MyVerifyChecksum(), MyIngress(), MyEgress(), MyComputeChecksum(), MyDeparser()) main;
+V1Switch(
+    MyParser(), 
+    MyVerifyChecksum(),
+    MyIngress(), 
+    MyEgress(),
+    MyComputeChecksum(),
+    MyDeparser()) main;
